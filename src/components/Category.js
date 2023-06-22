b75a63b38ecaef64d4a33f75f9b43ea1143d4e45
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2023-06-22 17:34:31 +0700

    done

---
+++ src/components/Category.js
@@ -0,0 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+
+export default function Category() {
+    let navigate = useNavigate();
+    const sendDataToProduct = event => {
+        navigate(`/product/${event.target.value}`);
+    };
+    return (
+        <div>
+            <h2>Select a Category:</h2>
+            <select defaultValue="default" onChange={e => sendDataToProduct(e)}>
+                <option value="default" disabled hidden>Choose your car</option>
+                <option value="1">Honda</option>
+                <option value="2">Suzuki</option>
+                <option value="3">Yamaha</option>
+                <option value="4">Vinfast</option>
+            </select>
+        </div>
+    );
+}
